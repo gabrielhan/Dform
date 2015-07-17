a0d77d168f170b902a68e74fbccd578fe7ff2043
gabrielhan <hannaby@intechinfo.fr>
2015-07-17 10:02:54 +0200

    start

---
+++ D-Form.Tests/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using D_Form;
+
+namespace D_Form.Tests
+{
+    [TestFixture]
+    public class Dform
+    {
+        [Test]
+        public void CreateAnswers()
+        {
+            Form f = new Form();
+            Assert.IsNull(f.Title);
+            f.Title = "jj";
+            Assert.AreEqual("jj", f.Title);
+
+            FormAnswer a = f.FindOrCreateAnswer("Emilie");
+            Assert.IsNotNull(a);
+            FormAnswer b = f.FindOrCreateAnswer("Emilie");
+            Assert.AreSame(a,b);
+
+            Assert.AreEqual(1, f.AnswerCount);
+            FormAnswer c = f.FindOrCreateAnswer("John Doe");
+            Assert.AreNotSame(a, c);
+
+            Assert.AreEqual("Emilie", a.UniqueName);
+            Assert.AreEqual("John Doe", c.UniqueName);
+        }
+
+        [Test]
+        public void CreateQuestionFolders()
+        {
+            Form f = new Form();
+            f.Questions.Title = "HG68-Bis";
+            Assert.AreEqual("HG68-Bis", f.Title);
+            BaseQuestion q1 = f.Questions.AddNewQuestion(typeof(PaneQuestion));
+            BaseQuestion q2 = f.Questions.AddNewQuestion("D_Form.PaneQuestion,D_Form");
+            Assert.AreEqual(0, q1.Index);
+            Assert.AreEqual(1, q2.Index);
+            q2.Index = 0;
+            Assert.AreEqual(0, q2.Index);
+            Assert.AreEqual(1, q1.Index);
+            q2.Parent = null;
+            Assert.AreEqual(0, q1.Index);
+            q2.Parent = q1;
+            Assert.IsTrue(f.Questions.Contains(q1));
+            Assert.IsTrue(f.Questions.Contains(q2));
+        }
+    }
+}
---
+++ D-Form/Dform.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_Form
+{
+    public class Form
+    {
+        private String _Label;
+        private Dictionary<String, FormAnswer> _Answers;
+        private RootQuestion _RootQuestion;
+
+        public Form()
+        {
+            _RootQuestion = new RootQuestion(this);
+            _Answers = new Dictionary<String, FormAnswer> (); 
+        }
+
+        public string Title 
+        {get{
+            if (_RootQuestion.Title != null)
+                    {
+                        return _RootQuestion.Title;
+                    }
+                    return _Label;
+        
+       } set { _Label = value; } }
+
+        public int AnswerCount { get { return _Answers.Count(); } }
+
+        public RootQuestion Questions { get { return _RootQuestion; } }
+
+        public FormAnswer FindOrCreateAnswer(string name)
+        {
+            FormAnswer a; 
+            _Answers.TryGetValue(name, out a);
+            if (a == null)
+            {
+                a = new FormAnswer(_Answers, name);
+            }
+            return a;
+        }
+
+
+    }
+
+    public class FormAnswer
+    {
+        private String _UniqueName;
+
+        public FormAnswer(Dictionary<String, FormAnswer> d, string name)
+        {
+            _UniqueName = name;
+            d.Add(name,this);
+        }
+
+        public string UniqueName{ get { return _UniqueName; } }
+
+
+    }
+
+    public abstract class BaseQuestion
+    {
+        BaseQuestion _Parent;
+        List<BaseQuestion> _Childs;
+        String _Title;
+        int _Index;
+
+        public int Index { get { return _Index; } set { _Index = value; } }
+        public string Title { get { return _Title; } set { _Title = value; } }
+        public BaseQuestion Parent { get { return _Parent; } set 
+        {
+            _Parent = value;
+            if (_Parent != null)
+            {
+                _Index = _Parent._Childs.Count();
+                _Parent.Childs.Add(this);
+            }
+        } }
+
+        public bool Contains(BaseQuestion q)
+        {
+            return true;
+        }
+
+        public BaseQuestion AddNewQuestion(String s)
+        {
+            BaseQuestion a = (BaseQuestion)Activator.CreateInstance(Type.GetType(s));
+            a.Parent = this; 
+            return a;
+        }
+
+    }
+
+    public class RootQuestion : BaseQuestion
+    {
+        private Form _Parent;
+        private String _Title;
+        private List<BaseQuestion> _Childs;
+
+        public RootQuestion(Form form)
+        {
+            _Parent = form;
+        }
+
+
+        public BaseQuestion AddNewQuestion(Type type)
+        {
+            return (BaseQuestion)Activator.CreateInstance(type);
+        }
+
+
+    }
+
+    public class PaneQuestion : BaseQuestion
+    {
+        private Form _Parent;
+        private String _Title;
+        private List<BaseQuestion> _Childs;
+    }
+}
